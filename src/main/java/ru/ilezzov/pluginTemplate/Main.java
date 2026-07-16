762a9ebb270bcfc7d3535cb6454d3657f8712480
ILeZzoV <ilezov.morozov@yandex.com>
2026-07-16 12:46:21 +0300

    feat(core): create Main class and initialize PluginLogger and ConsoleMessages

--- src/main/java/ru/ilezzov/pluginTemplate/Main.java
+++ src/main/java/ru/ilezzov/pluginTemplate/Main.java
@@ -1,18 +1,29 @@
 package ru.ilezzov.pluginTemplate;
 
+import lombok.Getter;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import ru.ilezzov.pluginTemplate.logger.ConsoleMessage;
+import ru.ilezzov.pluginTemplate.logger.PluginLogger;
+
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
 
 public final class Main extends JavaPlugin {
+    @Getter
+    private PluginLogger pluginLogger;
+
+    @Getter
+    private ConsoleMessage consoleMessage;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        this.getLogger().info("Hello!");
+        this.pluginLogger = new PluginLogger(this);
+        this.consoleMessage = new ConsoleMessage("messages");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
