634e2d71578d1152787a4853a90126149f4f79ec
ILeZzoV <ilezov.morozov@yandex.com>
2026-07-18 16:52:22 +0300

    refactor(config): change MessageFile access from constructor injection to provider method

--- src/main/java/ru/ilezzov/pluginTemplate/message/MessageManager.java
+++ src/main/java/ru/ilezzov/pluginTemplate/message/MessageManager.java
@@ -4,13 +4,13 @@ import lombok.AllArgsConstructor;
 import net.kyori.adventure.text.Component;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
+import ru.ilezzov.pluginTemplate.Main;
 import ru.ilezzov.pluginTemplate.file.MessageFile;
 import ru.ilezzov.pluginTemplate.placeholder.PluginPlaceholder;
 
 @AllArgsConstructor
 public class MessageManager {
-    private final Plugin plugin;
-    private final MessageFile messageFile;
+    private final Main plugin;
 
     public void send(final CommandSender commandSender, final String text, final PluginPlaceholder placeholder) {
         commandSender.sendMessage(
@@ -19,6 +19,10 @@ public class MessageManager {
     }
 
     private Component parseComponent(final String text, final PluginPlaceholder placeholder) {
-        return this.messageFile.colorizer.parse(text, placeholder);
+        return this.message().colorizer.parse(text, placeholder);
+    }
+
+    private MessageFile message() {
+        return this.plugin.getMessageFile();
     }
 }
