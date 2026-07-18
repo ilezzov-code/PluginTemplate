ae5d7f0f04f965fb2290f24e1ae4c25ad445b2cb
ILeZzoV <ilezov.morozov@yandex.com>
2026-07-18 18:00:06 +0300

    feat(messages): add sendFromThread method to MessageManager for thread-safe execution

--- src/main/java/ru/ilezzov/pluginTemplate/message/MessageManager.java
+++ src/main/java/ru/ilezzov/pluginTemplate/message/MessageManager.java
@@ -2,8 +2,10 @@ package ru.ilezzov.pluginTemplate.message;
 
 import lombok.AllArgsConstructor;
 import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 import ru.ilezzov.pluginTemplate.Main;
 import ru.ilezzov.pluginTemplate.file.MessageFile;
 import ru.ilezzov.pluginTemplate.placeholder.PluginPlaceholder;
@@ -16,6 +18,12 @@ public class MessageManager {
         commandSender.sendMessage(
                 this.parseComponent(text, placeholder)
         );
+    }
+
+    public void sendFromThread(final CommandSender sender, final String text ,final PluginPlaceholder placeholder) {
+        Bukkit.getScheduler().runTask(plugin, () -> {
+            this.send(sender, text, placeholder);
+        });
     }
 
     private Component parseComponent(final String text, final PluginPlaceholder placeholder) {
